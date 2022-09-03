8f47b880bed0769c9edac97fdae1b096eb79a7d5
victoria-parker <victoriaparkerb@gmail.com>
2022-09-03 10:01:00 +0200

    functionality added for changing nav bg color when scroll

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,13 @@ const body=document.querySelector('body')
 const header=document.querySelector('.header')
 
 
-    window.onscroll= ()=>{
+window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 1 || document.documentElement.scrollTop >= 1) {
         header.classList.add('header-scrolled')
+    } else {
+        header.classList.remove('header-scrolled')
     }
+};
+
+
